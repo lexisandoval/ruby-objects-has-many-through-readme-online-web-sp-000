848a1b0689516ff8cd19563dbd377111e4439848
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-03 20:19:11 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -21,4 +21,11 @@ class Waiter
     Meal.all.select {|meal| meal.waiter == self  }
   end
 
+  def best_tipper
+    # best_tipped_meal = meals.max { |meal_a, meal_b| meal_a.tip <=> meal_b.tip }
+    # best_tipped_meal.customer
+
+    best_arr = meals.sort
+  end
+
 end
