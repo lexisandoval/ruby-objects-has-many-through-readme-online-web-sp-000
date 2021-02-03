0c8764c43609740eccaea53ae6fcacda8118adef
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-03 19:55:22 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,22 @@
 class Customer
-  
+
+  attr_accessor :name, :age, :customer, :total, :tip, :waiter
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip)
+    @total = self
+    @tip = self
+    @waiter = self
+  end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,16 @@
 class Waiter
 
+  attr_accessor :name, :years
+  @@all = []
+
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
